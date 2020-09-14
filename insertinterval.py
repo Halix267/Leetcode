54897918084ea8c91ada3f9341745239828c62e8
Harshit Dhyani <63572018+Halix267@users.noreply.github.com>
2020-09-14 10:55:12 +0530

    Create insertinterval.py

---
+++ insertinterval.py
@@ -0,0 +1,38 @@
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+
+
+
+######################################### SOLUTION #################################################################################
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        
+        for interval in intervals:
+			# the new interval is after the range of other interval, so we can leave the current interval baecause the new one does not overlap with it
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            # the new interval's range is before the other, so we can add the new interval and update it to the current one
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            # the new interval is in the range of the other interval, we have an overlap, so we must choose the min for start and max for end of interval 
+            elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
+                newInterval[0] = (min(interval[0], newInterval[0]))
+                newInterval[1] = (max(newInterval[1], interval[1]))
+
+        
+        result.append(newInterval); 
+        return result
