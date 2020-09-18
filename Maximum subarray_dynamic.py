e287eb636208a7cdd415e68e700c3ae9b0dddd98
Harshit Dhyani <63572018+Halix267@users.noreply.github.com>
2020-09-18 11:18:29 +0530

    Create Maximum subarray_dynamic.py

---
+++ Maximum subarray_dynamic.py
@@ -0,0 +1,38 @@
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [0]
+Output: 0
+Example 4:
+
+Input: nums = [-1]
+Output: -1
+Example 5:
+
+Input: nums = [-2147483647]
+Output: -2147483647
+
+
+############################################################# Solution ##################################################################
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        _sum=[0]*len(nums)
+        _sum[0]=nums[0]
+        for i in range(1,len(nums)):
+            _sum[i]=max(nums[i],nums[i]+_sum[i-1])
+        _sum.sort()   
+        return _sum[-1]
