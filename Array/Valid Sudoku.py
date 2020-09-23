663bbd8f09420b5d7ee04d462734f7f329be8e5f
Harshit Dhyani <63572018+Halix267@users.noreply.github.com>
2020-09-23 10:41:01 +0530

    Create Valid Sudoku.py

---
+++ Array/Valid Sudoku.py
@@ -0,0 +1,97 @@
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+Example 2:
+
+Input:
+[
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+The given board contain only digits 1-9 and the character '.'.
+The given board size is always 9x9.
+
+######################################################################################## Solution ##################################################
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        if not board:
+            return True
+        if not self.checkRows(board):
+            return False
+        if not self.checkCols(board):
+            return False
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                if not self.checkRegion(i,j,board):
+                    return False
+        return True
+    
+    def checkRegion(self,x,y,grid):
+        regionElements = set()
+        for i in range(x,x+3):
+            for j in range(y,y+3):
+                if grid[i][j]!='.':
+                    if grid[i][j] in regionElements:
+                        return False
+                    else:
+                        regionElements.add(grid[i][j])
+        return True
+        
+    def checkCols(self,grid):
+        for j in range(9):
+            colElements = set()
+            for i in range(9):
+                if grid[i][j]!='.':
+                    if grid[i][j] in colElements:
+                        return False
+                    else:
+                        colElements.add(grid[i][j])
+        return True
+        
+    def checkRows(self,grid):
+        for i in range(9):
+            rowElements = set()
+            for v in grid[i]:
+                if v!='.':
+                    if v in rowElements:
+                        return False
+                    else:
+                        rowElements.add(v)
+        return True
