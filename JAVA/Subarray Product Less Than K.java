5b41486255afe58ffe780bbdeac9073a9f6e3e9f
Harshit Dhyani <63572018+Halix267@users.noreply.github.com>
2020-09-29 20:10:17 +0530

    Create  Subarray Product Less Than K.java

---
+++ JAVA/Subarray Product Less Than K.java
@@ -0,0 +1,36 @@
+Your are given an array of positive integers nums.
+
+Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+
+Example 1:
+Input: nums = [10, 5, 2, 6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+Note:
+
+0 < nums.length <= 50000.
+0 < nums[i] < 1000.
+0 <= k < 10^6.
+
+
+################################################### SOLUTION ##########################################################
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1){return 0;}
+        int total=0;
+        int i=0,j=0;
+        int p=1;
+        while(i<nums.length) {
+            p*=nums[i];
+            while(j<=i && p>=k) {
+                p/=nums[j];
+                j++;
+            }
+            total+=(i-j +1);
+            i++;
+        }
+        return total;
+        
+    }
+}
