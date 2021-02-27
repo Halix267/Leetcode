f6d291269631d90480e6edc2f1a0e21eb13e52e3
Harshit Dhyani <harshitdhyani575@gmail.com>
2021-02-27 18:46:34 +0530

    Create In-out time of graph.java

---
+++ Graph Theory/In-out time of graph.java
@@ -0,0 +1,19 @@
+int timer=1;
+boolean dfs(int v){
+
+  vis[v]=1;
+  In[v] = timer++;
+  
+  for(int child:l.get(v)){
+    
+    if(vis[child]==0){
+      
+      dfs(child);
+      
+    }
+    
+  }
+  
+  Out[v] = timer++;
+  
+}
